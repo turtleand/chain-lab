a9025394f28de67e69e8267ed3de238962c2b39f
Turtleand <turtliophant@gmail.com>
2026-08-23 20:46:40 -0300

    fix: canonicalize decentralized systems host (#33)
    Co-authored-by: Turtleand <hello@turtleand.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://chain.turtleand.com',
+  site: 'https://decentralized.turtleand.com',
   integrations: [mdx(), sitemap()],
   markdown: {
     smartypants: false
